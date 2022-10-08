eb1bd2460cc40340a3735476a39248b2b5c6672e
Alex Perez San Elias <alexirae@gmail.com>
2022-10-08 14:05:28 +0200

    Adjusted project to be compliant with UE 5.1 Preview.

--- Source/UEAudioDSPTemplateEditor.Target.cs
+++ Source/UEAudioDSPTemplateEditor.Target.cs
@@ -8,6 +8,7 @@ public class UEAudioDSPTemplateEditorTarget : TargetRules
     public UEAudioDSPTemplateEditorTarget( TargetInfo Target) : base(Target)
     {
         Type = TargetType.Editor;
+        IncludeOrderVersion  = EngineIncludeOrderVersion.Latest;
         DefaultBuildSettings = BuildSettingsVersion.V2;
         ExtraModuleNames.AddRange( new string[] { "UEAudioDSPTemplate" } );
     }
